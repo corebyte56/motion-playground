3cea1b60cc96605d42b1e44cbd8202d3b12cefbf
corebyte56 <fahim789m@gmail.com>
2026-03-24 23:08:34 +0600

    feat: display seafood meals with loading state and responsive grid

--- 01-framer-motion-basics/src/App.jsx
+++ 01-framer-motion-basics/src/App.jsx
@@ -24,13 +24,17 @@ const App = () => {
     <div className='grid grid-cols-2 lg:grid-cols-5 md:grid-cols-3 gap-9 container mx-auto py-13'>
       {info.length > 0 ? (
         info.map((meal) => (
-          <div key={meal.idMeal} className='p-7 border flex flex-col gap-5 items-center justify-between rounded-2xl'>
+          <div key={meal.idMeal} className='p-7 border flex flex-col gap-5 items-center justify-between rounded-2xl cursor-pointer'>
             <h2 className='text-[20px] font-medium'>{meal.strMeal}</h2>
             <img className='rounded-2xl' src={meal.strMealThumb} alt={meal.strMeal} width={200} />
           </div>
         ))
       ) : (
-        <p>No meals found</p>
+        <div className=' h-screen w-screen'>
+      <h3 className=' text-3xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-semibold'>
+        Loading...
+      </h3>
+    </div>
       )}
     </div>
   );
