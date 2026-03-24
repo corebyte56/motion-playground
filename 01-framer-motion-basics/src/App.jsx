bfdd62a7571ee603c2c04edbba285914394e7cc6
corebyte56 <fahim789m@gmail.com>
2026-03-25 00:33:37 +0600

    refactor: update seafood meal cards layout with button inside image section and flex row on large screens

--- 01-framer-motion-basics/src/App.jsx
+++ 01-framer-motion-basics/src/App.jsx
@@ -1,20 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import {motion} from 'motion/react'
-import { hover } from "motion"
+import React, { useEffect, useState } from "react";
+import { motion, useScroll } from "motion/react";
+import { hover } from "motion";
 
 const App = () => {
+  const { scrollYProgress } = useScroll();
   const [info, setInfo] = useState([]);
 
   const renderCards = async () => {
     try {
       const res = await fetch(
-        'https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood');
-      
+        "https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood",
+      );
+
       const data = await res.json();
       setInfo(data.meals || []);
       console.log(data.meals);
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error("Error fetching data:", error);
     }
   };
 
@@ -23,45 +25,60 @@ const App = () => {
   }, []);
 
   return (
-    <div className='grid grid-cols-2 lg:grid-cols-4 md:grid-cols-3 gap-9 container mx-auto py-13'>
-      {info.length > 0 ? (
-        info.map((meal) => (
-          <motion.div key={meal.idMeal} 
-          drag
-          dragConstraints = {{
-            top: 0,
-            right: 0,
-            bottom: 0,
-            left: 0
-          }}
-          whileHover={{
-            scale : 1.1,
-            transition : {
-              duration : 0.3
-            }
-          }}
-          className='bg-white shadow-2xl p-4 flex flex-col lg:flex-row gap-5 justify-between rounded-2xl cursor-pointer'>
+    <div>
+      <motion.div
+        style={{ scaleX: scrollYProgress, originX: 0 }}
+        className="bg-red-400 w-full h-1 fixed top-0 left-0 z-50 rounded-full"
+      ></motion.div>
 
-            <div className='flex flex-col gap-8 items-start'>
-              <img className='rounded-2xl w-full h-40 object-cover' src={meal.strMealThumb} alt={meal.strMeal} width={200} />
-              <button className='px-3 py-2 bg-green-600 rounded-2xl text-[16px] font-medium border border-transparent cursor-pointer'>Order Now</button>
-            </div>
-            
-            <div className='flex flex-col justify-between'>
-              <h2 className='text-[14px] font-bold'>{meal.strMeal}</h2>
-            <p className='font-medium text-[#454545]'>price : ${Math.floor(Math.random() * (1000 - 100 + 1)) + 100}</p>
-            </div>
-            
-          </motion.div>
-          
-        ))
-      ) : (
-        <div className=' h-screen w-screen'>
-      <h3 className=' text-3xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-semibold'>
-        Loading...
-      </h3>
-    </div>
-      )}
+      <div className="grid grid-cols-2 lg:grid-cols-4 md:grid-cols-3 gap-9 container mx-auto py-13">
+        {info.length > 0 ? (
+          info.map((meal) => (
+            <motion.div
+              key={meal.idMeal}
+              drag
+              dragConstraints={{
+                top: 0,
+                right: 0,
+                bottom: 0,
+                left: 0,
+              }}
+              whileHover={{
+                scale: 1.1,
+                transition: {
+                  duration: 0.3,
+                },
+              }}
+              className="bg-white shadow-2xl p-4 flex flex-col gap-5 justify-between rounded-2xl cursor-pointer"
+            >
+              <div className="flex flex-col gap-8 items-start">
+                <img
+                  className="rounded-2xl w-full h-40 object-cover"
+                  src={meal.strMealThumb}
+                  alt={meal.strMeal}
+                  width={200}
+                />
+              </div>
+
+              <div className="flex flex-col justify-between space-y-3">
+                <h2 className="text-[14px] font-bold">{meal.strMeal}</h2>
+                <p className="font-medium text-[#454545]">
+                  price : ${Math.floor(Math.random() * (1000 - 100 + 1)) + 100}
+                </p>
+                <button className="w-full px-4 py-3 bg-green-500 rounded-2xl text-white font-semibold">
+                  Order Now
+                </button>
+              </div>
+            </motion.div>
+          ))
+        ) : (
+          <div className=" h-screen w-screen">
+            <h3 className=" text-3xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-semibold">
+              Loading...
+            </h3>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
