31e1039f27c362458de37fdd894eafd97d02cd50
corebyte56 <fahim789m@gmail.com>
2026-03-25 22:39:15 +0600

    feat: add basic motion animation with x, y, and rotate using Framer Motion

--- 02-framer-motion-animation/src/App.jsx
+++ 02-framer-motion-animation/src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
-
+import {motion} from 'motion/react'
 const App = () => {
   return (
-    <div>
-      app
-    </div>
+    <motion.div 
+    initial = {{
+      x: 100,
+      y:0
+    }}
+    animate = {{
+      x:[100,1000,1000,100,100],
+      y:[0,0,600,600,0],
+      rotate:[180,260,360,45,180]
+    }}
+  
+    transition={{
+      duration:3,
+      delay:0.4
+    }}
+    className='text-white bg-indigo-500 h-70 w-70 border-3 border-white '>
+      
+    </motion.div>
   )
 }
 
