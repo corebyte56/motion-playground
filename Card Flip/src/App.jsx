dbf132a9122906ea6483a57c3906382d41e9956d
corebyte56 <fahim789m@gmail.com>
2026-03-25 23:31:12 +0600

    feat: add continuously rotating 3D card with purple color on black background using framer-motion

--- Card Flip/src/App.jsx
+++ Card Flip/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { motion } from 'framer-motion'
+import { motion, hover } from 'framer-motion'
 
 const App = () => {
   return (
@@ -8,9 +8,18 @@ const App = () => {
       style={{ perspective: '1000px' }} // 3D perspective
     >
       <motion.div 
-        animate={{ rotateY: 360 }}
-        transition={{ duration: 2, repeat: Infinity }}
-        className='h-[200px] w-[200px] bg-green-700/70 rounded-2xl border-2 border-white flex items-center justify-center text-white font-bold'
+       
+        animate={{
+          rotateY:360
+          
+        }}
+        transition = {{
+          duration : 0.6,
+          delay : 0.3,
+          repeat: Infinity
+        }}
+
+        className='h-[200px] w-[200px] bg-purple-500 rounded-2xl border-2 border-white flex items-center justify-center text-white font-bold'
       >
         CARD
       </motion.div>
