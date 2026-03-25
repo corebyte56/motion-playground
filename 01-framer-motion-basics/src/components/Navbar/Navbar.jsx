5b925994a9b25f0934b131cef1fb39510e94d389
corebyte56 <fahim789m@gmail.com>
2026-03-25 17:06:53 +0600

    feat: add responsive mobile menu toggle with lucide-react icons

--- 01-framer-motion-basics/src/components/Navbar/Navbar.jsx
+++ 01-framer-motion-basics/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,10 @@
 import { motion, hover } from "motion/react";
+import { X, Menu } from "lucide-react";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [ShowMenu, setShowMenu] = useState(false);
+
   return (
     <motion.div
       initial={{ y: -80, opacity: 0 }}
@@ -24,30 +28,84 @@ const Navbar = () => {
         </div>
 
         {/* Right side */}
-        <div className ="flex items-center gap-4">
+        <div className="flex items-center gap-4">
           <motion.button
-          whileTap={{
-                    opacity: 0.1,
-                    scale: 0.2,
-                  }}
-                  transition={{
-                    
-                  }}
-          className="hidden sm:block px-4 py-2 rounded-2xl border border-green-500 text-green-600 cursor-pointer hover:bg-green-500 hover:text-white transition">
+            whileTap={{
+              opacity: 0.1,
+              scale: 0.2,
+            }}
+            transition={{}}
+            className="hidden sm:block px-4 py-2 rounded-2xl border border-green-500 text-green-600 cursor-pointer hover:bg-green-500 hover:text-white transition"
+          >
             Login
           </motion.button>
 
           <motion.button
-          whileTap={{
-                    opacity: 0.1,
-                    scale: 0.2,
-                  }}
-                  transition={{
-                    
-                  }}
-          className="px-4 py-2 border border-transparent bg-green-500 text-white rounded-2xl cursor-pointer hover:bg-transparent hover:text-green-600 hover:border hover:border-green-500 transition shadow-md">
+            whileTap={{
+              opacity: 0.1,
+              scale: 0.2,
+            }}
+            transition={{}}
+            className="px-4 py-2 border border-transparent bg-green-500 text-white rounded-2xl cursor-pointer hover:bg-transparent hover:text-green-600 hover:border hover:border-green-500 transition shadow-md"
+          >
             Sign Up
           </motion.button>
+        </div>
+
+        {/* resposive Menu */}
+
+        <div className="md:hidden cursor-pointer">
+          <motion.button
+            whileTap={{
+              opacity: 0.5,
+            }}
+            transition={{
+              duration: 0.6,
+            }}
+            onClick={() => {
+              setShowMenu(!ShowMenu);
+            }}
+            className="cursor-pointer"
+          >
+            {ShowMenu ? <X /> : <Menu />}
+          </motion.button>
+
+          <div className="hidden flex flex-col items-center gap-4 absolute top-0 text-gray-700 font-medium">
+            <p className="hover:text-green-600 cursor-pointer transition">
+              Home
+            </p>
+            <p className="hover:text-green-600 cursor-pointer transition">
+              Menu
+            </p>
+            <p className="hover:text-green-600 cursor-pointer transition">
+              About
+            </p>
+          </div>
+
+          <div className="hidden flex items-center gap-4">
+          <motion.button
+            whileTap={{
+              opacity: 0.1,
+              scale: 0.2,
+            }}
+            transition={{}}
+            className="hidden sm:block px-4 py-2 rounded-2xl border border-green-500 text-green-600 cursor-pointer hover:bg-green-500 hover:text-white transition"
+          >
+            Login
+          </motion.button>
+
+          <motion.button
+            whileTap={{
+              opacity: 0.1,
+              scale: 0.2,
+            }}
+            transition={{}}
+            className="px-4 py-2 border border-transparent bg-green-500 text-white rounded-2xl cursor-pointer hover:bg-transparent hover:text-green-600 hover:border hover:border-green-500 transition shadow-md"
+          >
+            Sign Up
+          </motion.button>
+        </div>
+
         </div>
       </div>
     </motion.div>
