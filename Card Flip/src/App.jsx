b8c27223cce819b2c8df10aadf67aa96f63592af
corebyte56 <fahim789m@gmail.com>
2026-03-25 23:23:51 +0600

    feat: add rotating 3D card using framer-motion

--- Card Flip/src/App.jsx
+++ Card Flip/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 const App = () => {
   return (
-    <div>
-      
+    <div 
+      className='flex items-center justify-center h-screen bg-black' 
+      style={{ perspective: '1000px' }} // 3D perspective
+    >
+      <motion.div 
+        animate={{ rotateY: 360 }}
+        transition={{ duration: 2, repeat: Infinity }}
+        className='h-[200px] w-[200px] bg-green-700/70 rounded-2xl border-2 border-white flex items-center justify-center text-white font-bold'
+      >
+        CARD
+      </motion.div>
     </div>
   )
 }
